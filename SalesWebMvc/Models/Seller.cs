ec36056847671d9fb9301d759861475ddf3d9e0a
Felipe <lipesmaffini@gmail.com>
2023-11-30 23:19:00 -0400

    Create Seller

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -20,6 +20,7 @@ namespace SalesWebMvc.Models
 
         [ForeignKey("DepartmentId")]
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
